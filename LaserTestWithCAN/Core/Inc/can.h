ae5e61951db8c8686e8f0fb78943e6bea843ef86
czagaadam <czagaadam@gmail.com>
2024-05-16 14:05:11 +0200

    Transfer VL53 I2C sensor data via CAN

---
+++ LaserTestWithCAN/Core/Inc/can.h
@@ -0,0 +1,23 @@
+/*
+ * can.h
+ *
+ *  Created on: May 14, 2024
+ *      Author: AMD_FX_X8
+ */
+
+#ifndef INC_CAN_H_
+#define INC_CAN_H_
+#ifdef __cplusplus
+ extern "C" {
+#endif
+
+
+#include "stm32f4xx_hal.h"
+#include "stm32f4xx_hal_can.h"
+
+void MX_CAN1_Init(void);
+
+#ifdef __cplusplus
+}
+#endif
+#endif /* INC_CAN_H_ */
